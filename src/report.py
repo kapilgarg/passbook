35ff69c4131bba7571691464aafdb672bdb10ac2
KGarg <kapil.garg@sevenlakes.com>
2020-09-07 12:01:39 +0530

    fixed email report - initial version

--- src/report.py
+++ src/report.py
@@ -1,4 +1,7 @@
-from dateutil.parser import parse
+"""
+report module
+"""
+from string import Template
 import itertools
 import logging
 import gsheet
@@ -8,24 +11,77 @@ logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def generate(spreadsheet_id, sheet_name, date):
+mail_template = Template("""
+Hello,
+Here is your expense report.
+
+Total expense : $total_expense
+---------------------------------------------------
+$current_day_records
+
+Categorized Expense
+---------------------------------------------------
+$expense_by_category_daily
+
+Total expense in this month (till now)
+---------------------------------------------------
+$expense_by_category_monthly
+""")
+line_record_seller = Template("INR $amount - $seller \n")
+line_record_category = Template("INR $amount - $category \n")
+
+def __generate_data(spreadsheet_id, sheet_name, date):
     records = gsheet.get_from_sheet(spreadsheet_id, sheet_name)
     current_day_record = [record for record in records if record[0] == date.strftime("%Y-%m-%d")]
     current_day_record  = sorted(current_day_record,key=lambda x:x[4])
     group_by_category_daily = itertools.groupby(current_day_record, lambda x:x[4])
 
-    expanse_by_category_daily = {}
+    expense_by_category_daily = {}
     for category,group in group_by_category_daily:
-        expanse_by_category_daily[category] = sum([float(rec[1]) for rec in group])
+        expense_by_category_daily[category] = sum([float(rec[1]) for rec in group])
     
-    expanse_by_category_monthly = {}
+    expense_by_category_monthly = {}
     monthly_records = sorted(records,key=lambda x:x[4])
     group_by_category_month = itertools.groupby(monthly_records, lambda x:x[4])
     for category,group in group_by_category_month:
-        expanse_by_category_monthly[category] = sum([float(rec[1]) for rec in group])
-
-    return {
-        'current_day_record':current_day_record,
-        'expanse_by_category_daily': expanse_by_category_daily,
-        'expanse_by_category_monthly':expanse_by_category_monthly
+        expense_by_category_monthly[category] = sum([float(rec[1]) for rec in group])
+    
+    return {        
+        'current_day_records':current_day_record,
+        'expense_by_category_daily': expense_by_category_daily,
+        'expense_by_category_monthly':expense_by_category_monthly
     }
+
+def generate(spreadsheet_id, sheet_name, date):
+    """
+    returns the data to generate email notification
+    """
+    data = __generate_data(spreadsheet_id, sheet_name, date)
+
+    current_day_records = data['current_day_records']
+    expense_by_category_daily = data['expense_by_category_daily']
+    expense_by_category_monthly = data['expense_by_category_monthly']
+
+    #total expense contains only debit
+    total_expense = sum([float(record[1]) for record in current_day_records if float(record[1]) > 0])
+    for record in current_day_records:
+        expense_for_current_day = expense_for_current_day + line_record_seller.substitute(amount=record[1], seller=record[2])
+    logger.debug(expense_for_current_day)
+
+    expense_by_category_daily_str = ''
+    for category, expense_amount in expense_by_category_daily.items():
+        expense_by_category_daily_str = line_record_category.substitute(amount=expense_amount, category=category)
+    logger.debug(expense_by_category_daily_str)
+
+    expense_by_category_monthly_str = ''
+    for category, expense_amount in expense_by_category_monthly.items():
+        expense_by_category_monthly_str = line_record_category.substitute(amount=expense_amount, category=category)
+    logger.debug(expense_by_category_monthly_str)
+
+    return mail_template.substitute({
+        'total_expense':total_expense,
+        'date':date.strftime("%Y-%m-%d"),
+        'current_day_records':expense_for_current_day,
+        'expense_by_category_daily':expense_by_category_daily_str,
+        'expense_by_category_monthly':expense_by_category_monthly_str
+    })
--- src/send_mail.py
+++ src/send_mail.py
@@ -1,7 +1,6 @@
 """Send an email message from the user's account.
 """
 
-from datetime import datetime
 import base64
 from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
@@ -11,13 +10,18 @@ from email.mime.text import MIMEText
 import mimetypes
 import os
 from googleapiclient.discovery import build
-from apiclient import errors
 from google_auth import creds
 
 
 service = build('gmail', 'v1', credentials=creds)
 
-def send_message(user_id, email_message):
+
+def send_message(subject, message_text, to='me'):
+    message = create_message('me', to, subject, message_text)
+    __send_message(to, message)
+
+
+def __send_message(user_id, email_message):
     """Send an email message.
 
     Args:
@@ -29,80 +33,76 @@ def send_message(user_id, email_message):
     Returns:
       Sent Message.
     """
-    try:
-        message = (service.users().messages().send(userId=user_id, body=email_message).execute())
-        print('Message Id: %s' % message['id'])
-        return message
-    except errors.HttpError as  error:
-        print('An error occurred: %s' % error)
+    message = (service.users().messages().send(
+        userId=user_id, body=email_message).execute())
+    return message
 
 
 def create_message(sender, to, subject, message_text):
-  """Create a message for an email.
+    """Create a message for an email.
 
-  Args:
-    sender: Email address of the sender.
-    to: Email address of the receiver.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
 
-  Returns:
-    An object containing a base64url encoded email object.
-  """
-  message = MIMEText(message_text)
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-  #return {'raw': base64.urlsafe_b64encode(message.as_string().encode())}
-  return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
 
 def create_message_with_attachment(
-    sender, to, subject, message_text, file):
-  """Create a message for an email.
+        sender, to, subject, message_text, file):
+    """Create a message for an email.
 
-  Args:
-    sender: Email address of the sender.
-    to: Email address of the receiver.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
-    file: The path to the file to be attached.
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+      file: The path to the file to be attached.
 
-  Returns:
-    An object containing a base64url encoded email object.
-  """
-  message = MIMEMultipart()
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
 
-  msg = MIMEText(message_text)
-  message.attach(msg)
+    msg = MIMEText(message_text)
+    message.attach(msg)
 
-  content_type, encoding = mimetypes.guess_type(file)
+    content_type, encoding = mimetypes.guess_type(file)
 
-  if content_type is None or encoding is not None:
-    content_type = 'application/octet-stream'
-  main_type, sub_type = content_type.split('/', 1)
-  if main_type == 'text':
-    fp = open(file, 'rb')
-    msg = MIMEText(fp.read(), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'image':
-    fp = open(file, 'rb')
-    msg = MIMEImage(fp.read(), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'audio':
-    fp = open(file, 'rb')
-    msg = MIMEAudio(fp.read(), _subtype=sub_type)
-    fp.close()
-  else:
-    fp = open(file, 'rb')
-    msg = MIMEBase(main_type, sub_type)
-    msg.set_payload(fp.read())
-    fp.close()
-  filename = os.path.basename(file)
-  msg.add_header('Content-Disposition', 'attachment', filename=filename)
-  message.attach(msg)
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(file, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
 
-  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
